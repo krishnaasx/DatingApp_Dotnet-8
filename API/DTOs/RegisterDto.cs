109ada4dc1ba771a96f9834c5fc02453b02fdb50
krish <krishnaasinghbisht@gmail.com>
2024-08-26 03:10:55 +0530

    Commit_6

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.DTOs {
     public class RegisterDto {
-        
-        public required string Username { get; set; }
-        public required string Password { get; set; }
+        public string Username { get; set; } = string.Empty;
+
+        [StringLength(8, MinimumLength = 4)]
+        public string Password { get; set; } = string.Empty;
     }
 }
