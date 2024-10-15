e173e85c66273f608c891e67da19ae8bf64b7d38
krish <krishnaasinghbisht@gmail.com>
2024-10-15 23:49:48 +0530

    commit-11

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,6 +1,5 @@
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 import { inject, Injectable } from '@angular/core';
-import { AccountService } from "./account.service";
 import { environment } from "../../environments/environment";
 import { Member } from "../_models/member";
 
@@ -10,23 +9,15 @@ import { Member } from "../_models/member";
 export class MembersService {
 
   private http = inject(HttpClient);
-  private accountService = inject(AccountService);
   baseUrl = environment.apiUrl;
   constructor() { }
 
   getMemebers() {
-    return this.http.get<Member[]>(this.baseUrl + 'users', this.getHttpOptions());
+    return this.http.get<Member[]>(this.baseUrl + 'users');
   }
 
   getMember(username: string) {
-    return this.http.get<Member>(this.baseUrl + 'users/' + username, this.getHttpOptions());
+    return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
   
-  getHttpOptions() {
-    return {
-      headers : new HttpHeaders({
-        Authorization: `Bearer ${this.accountService.currentUser()?.token}`
-      })
-    }
-  }
 }
