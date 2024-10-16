f3314d33fc7a0fa9c598b1e909ec2bf6779cd5e2
krish <krishnaasinghbisht@gmail.com>
2024-10-16 19:12:25 +0530

    commit-15

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,17 +1,21 @@
-using System.Security.Claims;
+using API.Data;
 using API.DTOs;
+using API.Entities;
+using API.Extensions;
 using API.Interface;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace API.Controllers  {
+namespace API.Controllers {
+    
 
     [Authorize]
-    public class UsersController(IUserRespository userRespository, IMapper mapper) : BaseApiController {
+    public class UsersController(IUserRespository userRespository, IMapper mapper, IPhotoService photoService) : BaseApiController {
 
         [HttpGet]
         public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers() {
+
             var users = await userRespository.GetMembersAsync();
             var usersToRetrun = mapper.Map<IEnumerable<MemberDto>>(users);
             return Ok(usersToRetrun);
@@ -19,6 +23,7 @@ namespace API.Controllers  {
 
         [HttpGet("{username}")]
         public async Task<ActionResult<MemberDto>> GetUser(string username) {
+
             var user = await userRespository.GetUserByUsernameAsync(username);
             if (user == null) return NotFound();
             return mapper.Map<MemberDto>(user);
@@ -27,13 +32,50 @@ namespace API.Controllers  {
         [HttpPut]
         public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto) {
 
-            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-            if (username == null) return BadRequest("No username found in token");
-            var user = await userRespository.GetUserByUsernameAsync(username);
+            var user = await userRespository.GetUserByUsernameAsync(User.GetUsername());
             if(user == null) return BadRequest("Count not find the user");
             mapper.Map(memberUpdateDto, user);
             if (await userRespository.SaveAllAsync()) return NoContent();
             return BadRequest("Failed to update the user");
         }
+
+        [HttpPost("add-photo")]
+        public async Task<ActionResult<PhotoDto>> AddPhoto(IFormFile file) {
+
+            var user = await userRespository.GetUserByUsernameAsync(User.GetUsername());
+            if(user == null) return BadRequest("Cannot update user");
+            var result = await photoService.AddPhotoAsync(file);
+            if(result.Error != null) return BadRequest(result.Error.Message);
+
+            var photo = new Photo {
+                Url = result.SecureUrl.AbsoluteUri,
+                PublicId = result.PublicId
+            };
+
+            user.Photos.Add(photo);
+            
+            if (await userRespository.SaveAllAsync()) 
+                return CreatedAtAction(nameof(GetUser), 
+                    new {username=user.UserName}, mapper.Map<PhotoDto>(photo));
+
+            return BadRequest("Problem adding photo");
+        }
+
+        [HttpPut("set-main-photo/{photoId:int}")]
+        public async Task<ActionResult> SetMainPhoto(int photoId) {
+            var user = await userRespository.GetUserByUsernameAsync(User.GetUsername());
+            if (user == null) return BadRequest("Count not find the user");
+            var photo = user.Photos.FirstOrDefault(x => x.Id == photoId);
+            if (photo == null || photo.IsMain) return BadRequest("Cannot use this as main photo");
+            var currentMain = user.Photos.FirstOrDefault(x => x.IsMain);
+            if(currentMain != null) currentMain.IsMain = false;
+            photo.IsMain = true;
+
+            if (await userRespository.SaveAllAsync()) return NoContent();
+            return BadRequest("Problem setting main photo");
+        }
+
+        
+
     }
 }
--- API/Extensions/AppliacationServiceExtensions.cs
+++ API/Extensions/AppliacationServiceExtensions.cs
@@ -1,6 +1,8 @@
 using API.Data;
+using API.Helpers;
 using API.Interface;
 using API.Service;
+using API.Services;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Extensions {
@@ -14,7 +16,9 @@ namespace API.Extensions {
             services.AddCors();
             services.AddScoped<ITokenService, TokenService>();
             services.AddScoped<IUserRespository, UserRepository>();
+            services.AddScoped<IPhotoService, PhotoService>();
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
 
             return services;
         }
