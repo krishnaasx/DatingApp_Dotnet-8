4835bf4e6dd9dd3b8bb57c4d804ca24dd75cf37a
krish <krishnaasinghbisht@gmail.com>
2024-10-24 19:30:23 +0530

    set for production

--- API/Middleware/ExceptionMiddleware.cs
+++ API/Middleware/ExceptionMiddleware.cs
@@ -2,31 +2,35 @@ using System.Net;
 using System.Text.Json;
 using API.Errors;
 
-namespace API.Middleware {
-    public class ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger, IHostEnvironment env) {
-        
-        public async Task InvokeAsync(HttpContext context) {
+namespace API.Middleware;
 
-            try {
-                await next(context);
-            }
-            catch (Exception ex) {
+public class ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger, 
+    IHostEnvironment env)
+{
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, ex.Message);
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
 
-                logger.LogError(ex, ex.Message);
-                context.Response.ContentType = "application/json";
-                context.Response.StatusCode = (int) HttpStatusCode.InternalServerError;
+            var response = env.IsDevelopment()
+                ? new ApiException(context.Response.StatusCode, ex.Message, ex.StackTrace)
+                : new ApiException(context.Response.StatusCode, ex.Message, "Internal server error");
 
-                var response = env.IsDevelopment()
-                    ? new ApiException(context.Response.StatusCode, ex.Message, ex.StackTrace)
-                    : new ApiException(context.Response.StatusCode, ex.Message, "Internal server error");
+            var options = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
 
-                var options = new JsonSerializerOptions {
-                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase
-                };
+            var json = JsonSerializer.Serialize(response, options);
 
-                var json = JsonSerializer.Serialize(response, options);
-                await context.Response.WriteAsync(json);
-            }
+            await context.Response.WriteAsync(json);
         }
     }
 }
